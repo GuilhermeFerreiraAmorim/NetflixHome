a1579c221f98041a833bf9a51d899e9aa5a9c968
Guilherme Ferreira <guillhermeamorym@gmail.com>
2020-09-02 04:36:16 -0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,142 @@
+$(function(){
+
+    var ex = $('#box');
+
+    $('#bnt').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+    
+
+});
+
+$(function(){
+
+    var ex = $('#box2');
+
+    $('#bnt2').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+$(function(){
+
+    var ex = $('#box3');
+
+    $('#bnt3').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+
+$(function(){
+
+    var ex = $('#box4');
+
+    $('#bnt4').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+
+$(function(){
+
+    var ex = $('#box5');
+
+    $('#bnt5').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+
+$(function(){
+
+    var ex = $('#box6');
+
+    $('#bnt6').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+
+$(function(){
+
+    var ex = $('#box7');
+
+    $('#bnt7').click(function(){
+
+        ex.animate({
+
+            height: 'toggle'
+
+
+
+        }, 'fast');
+
+
+    });
+
+});
+
+
+
+
+    
